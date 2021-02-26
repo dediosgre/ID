680871022d16f32ab3781ada77e1d55aa3daac75
dediosgre <grex_ded@unicah.edu>
2021-02-26 15:31:20 -0600

    version 1.2 slider change

--- assets/js/car.js
+++ assets/js/car.js
@@ -10,18 +10,18 @@ window.addEventListener('load', function(){
   responsive: [
     {
       // screens greater than >= 775px
-      breakpoint: 800,
+      breakpoint: 450,
       settings: {
         // Set to `auto` and provide item width to adjust to viewport
-        slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToShow: 1,
+        slidesToScroll: 1,
       }
     },{
       // screens greater than >= 1024px
-      breakpoint: 1024,
+      breakpoint: 800,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 1,
+        slidesToScroll: 2,
         itemWidth: 150,
         duration: 0.25
       }
